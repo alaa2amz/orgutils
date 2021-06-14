18eb8d12afa322c4492afe178b15214f426b19f0
Your work <alaa2amz@gmail.com>
2021-06-14 12:58:42 +0200

    after loop and working

--- iter-nm.sh
+++ iter-nm.sh
@@ -26,25 +26,38 @@ for f in "$@";do
 	printf "\n%s" "$(basename "$f")" > $draft
 $viewer "$f" 2>/dev/null&
 pid=$!
-$editor $draft
-ii=$(cat $draft|tr '\n' '.' |sed 's/[^[:alnum:].,]/_/g'|sed 's/_*\._*/./g'|sed -E 's/^[\._]|[\._]$//g'|tr '[:upper:]' '[:lower:]'|tr -s '._')
-dirname=$(dirname "$f")
 
-if [ "$prefix" != "" ];then
-	mkdir $dirname/$prefix
-	dirname="$dirname/$prefix"
-fi
-echo -e "$counter / $# mv $f
+while [ : ];do 
+	$editor $draft
+	ii=$(cat $draft|tr '\n' '.' |sed 's/[^[:alnum:].,]/_/g'|sed 's/_*\._*/./g'|sed -E 's/^[\._]|[\._]$//g'|tr '[:upper:]' '[:lower:]'|tr -s '._')
+	dirname=$(dirname "$f")
+
+	if [ "$prefix" != "" ];then
+		mkdir -p $dirname/$prefix
+		dirname="$dirname/$prefix"
+	fi
+	tput clear;
+	echo -e "\x1b[33m${counter:=1} / $# \x1b[m
+mv 
+$f
 --to--
-\x1b[31m$dirname/$ii\x1b[m ??"
-read ok
-case $ok in
-	[yY]*)
-		mv -iv "$f" "$dirname/$ii" 2>&1|tee -a "$0"-log.txt; date >> "$0"-log.txt ;echo '-----' >>"$0"-log.txt
-		;;	
-	#e*) 
+\x1b[31m$dirname/\n\x1b[32m$ii\x1b[m ??
+-----------------------------
+y:proceed  e:reedit  x:exit
+anything else to skip
+-----------------------------"|fold -w60
 
-esac
+	read ok
+
+	case $ok in
+		y)
+		mv -iv "$f" "$dirname/$ii" 2>&1|tee -a "$0"-log.txt; date >> "$0"-log.txt ;echo '-----' >>"$0"-log.txt;break
+		;;	
+		e) continue ;;
+		x) kill $pid;exit;;
+		*)break;;
+	esac
+done
 
 kill $pid
 counter=$(($counter+1))
